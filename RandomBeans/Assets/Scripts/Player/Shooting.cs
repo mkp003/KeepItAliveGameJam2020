a194435228e1ea99ca7d5479516fb89803ec0e0d
tinyeye-zach <40218828+tinyeye-zach@users.noreply.github.com>
2020-05-16 16:49:08 -0600

    Player V0.1

---
+++ RandomBeans/Assets/Scripts/Player/Shooting.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+
+public class Shooting : MonoBehaviour
+{
+
+    public Transform firePoint;
+
+    public int damage = 20;
+
+    public GameObject impactEffect;
+
+    public LineRenderer lineRenderer;
+
+    Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+
+    // Raycast version!!!
+    IEnumerator Shoot()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.up);
+
+        if (hitInfo)
+        {
+            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            GameObject effect = Instantiate(impactEffect, hitInfo.point, Quaternion.identity);
+            Destroy(effect, 0.5f);
+
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+        } else
+        {
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, firePoint.position + firePoint.up * 100);
+        }
+
+        animator.SetTrigger("Shoot");
+
+        lineRenderer.enabled = true;
+
+        yield return new WaitForSeconds(0.02f);
+
+        lineRenderer.enabled = false;
+        
+    }
+}
