5f21142a0148bda218418e8e40d1f74435bec32c
Mack <mack.power1@gmail.com>
2020-05-16 14:52:33 -0600

    Added boundries on the map.

--- RandomBeans/Assets/Scripts/LevelGenerator.cs
+++ RandomBeans/Assets/Scripts/LevelGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
@@ -16,6 +17,11 @@ public class LevelGenerator : MonoBehaviour
     [Tooltip("Tile to create the map with")]
     [SerializeField]
     private GameObject mapTile;
+    [SerializeField]
+    private Sprite mapTileSideTopSprite;
+    [SerializeField]
+    private Sprite mapTileCornerSprite; // Top right
+
 
     [Tooltip("Container which holds the sprite tiles")]
     [SerializeField]
@@ -50,6 +56,7 @@ public class LevelGenerator : MonoBehaviour
         if (levelDimensionX > 5 || levelDimensionY > 5)
         {
             CreateBackground();
+            
             CreateObstacles();
         }
         else
@@ -70,6 +77,62 @@ public class LevelGenerator : MonoBehaviour
             {
                 GameObject newMapTile = Instantiate(mapTile, mapTileContainer.transform);
                 newMapTile.transform.localPosition = new Vector2(i, j);
+                AdjustSprite(newMapTile, i, j);
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="tile"></param>
+    /// <param name="currentX"></param>
+    /// <param name="currentY"></param>
+    private void AdjustSprite(GameObject tile, int currentX, int currentY)
+    {
+        bool xEdge = (currentX == 0 || currentX == levelDimensionX - 1);
+        bool yEdge = (currentY == 0 || currentY == levelDimensionY - 1);
+        if (!xEdge && !yEdge)
+            return;
+        if(currentX == 0 && currentY == 0)
+        {
+            tile.GetComponent<SpriteRenderer>().sprite = mapTileCornerSprite;
+            tile.transform.Rotate(0, 0, 180);
+        }
+        else if (currentX == 0 && currentY == levelDimensionY - 1)
+        {
+            tile.GetComponent<SpriteRenderer>().sprite = mapTileCornerSprite;
+            tile.transform.Rotate(0, 0, 90);
+        }
+        else if (currentY == 0 && currentX == levelDimensionX - 1)
+        {
+            tile.GetComponent<SpriteRenderer>().sprite = mapTileCornerSprite;
+            tile.transform.Rotate(0, 0, -90);
+        }
+        else if (currentX == levelDimensionX - 1 && currentY == levelDimensionY - 1)
+        {
+            tile.GetComponent<SpriteRenderer>().sprite = mapTileCornerSprite;
+        }
+
+        else if (xEdge)
+        {
+            tile.GetComponent<SpriteRenderer>().sprite = mapTileSideTopSprite;
+            if (currentX == 0)
+            {
+                tile.transform.Rotate(0, 0, 90);
+            }
+            else
+            {
+                tile.transform.Rotate(0, 0, -90);
+            }
+        }
+        else if(yEdge)
+        {
+            tile.GetComponent<SpriteRenderer>().sprite = mapTileSideTopSprite;
+            if (currentY == 0)
+            {
+                tile.transform.Rotate(0, 0, 180);
             }
         }
     }
@@ -88,6 +151,7 @@ public class LevelGenerator : MonoBehaviour
             Vector2 obstaclePosition = new Vector2(Random.Range(0, levelDimensionX), Random.Range(0, levelDimensionY));
             GameObject newObstacle = Instantiate(obstacles[type], obstacleContainer.transform);
             newObstacle.transform.localPosition = new Vector2(obstaclePosition.x, obstaclePosition.y);
+            
         }
     }
 }
